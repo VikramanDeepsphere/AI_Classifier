ed8cbb5f236adb5012837def73e9437567002003
VikramanSubramaniyan <svikraman010101@gmail.com>
2023-05-21 11:04:30 +0530

    ...

--- app.py
+++ app.py
@@ -27,8 +27,8 @@ with imcol2:
 with imcol3:
     st.write("")
 st.markdown("<p style='text-align: center; color: black; font-size:25px;'><span style='font-weight: bold'>ChatGPT Application: </span>Content Classifier</p>", unsafe_allow_html=True)
-st.markdown("<div style='display: flex; margin-top: -45px ; justify-content: center;'><hr style='height: 2px; background-color: gray; width: 440px; border: none;'></div>", unsafe_allow_html=True)
-st.markdown("<p style='text-align: center; color: black;margin-top: -10px ;font-size:20px;'><span style='font-weight: bold'></span>Classify Whether a Given Content is Written by Human or AI</p>", unsafe_allow_html=True)
+# st.markdown("<div style='display: flex; margin-top: -45px ; justify-content: center;'><hr style='height: 2px; background-color: gray; width: 440px; border: none;'></div>", unsafe_allow_html=True)
+st.markdown("<p style='text-align: center; color: blue;margin-top: -10px ;font-size:20px;'><span style='font-weight: bold'></span>Classify Whether a Given Content is Written by Human or AI</p>", unsafe_allow_html=True)
 st.markdown("<hr style=height:2.5px;background-color:gray;>",unsafe_allow_html=True)
 #---------Side bar-------#
 
